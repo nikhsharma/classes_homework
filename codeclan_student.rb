36f5804b972fc5a153d1458697423dafa8767e89
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-02 15:48:17 +0100

    add getters that return name and cohort

---
+++ codeclan_student.rb
@@ -0,0 +1,17 @@
+class CodeclanStudent
+
+def initialize(name, cohort)
+@name = name
+@cohort = cohort
+end
+
+def name
+  return @name
+end
+
+def cohort
+  return @cohort
+end
+
+
+end
--- specs/codeclan_student_spec.rb
+++ specs/codeclan_student_spec.rb
@@ -1,3 +1,19 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../codeclan_student.rb')
+
+class TestCodeclanStudent < MiniTest::Test
+
+def setup
+@student = CodeclanStudent.new("Nikhil", 5)
+end
+
+def test_student_name
+  assert_equal("Nikhil", @student.name)
+end
+
+def test_student_cohort
+  assert_equal(5, @student.cohort)
+end
+
+end
